96edc8f5a431c42aa505d9855974b689be850045
gmchen <gregorymchen@gmail.com>
2017-05-25 14:24:05 -0400

    Revert "Code to get rid of genefu dependency"
    This reverts commit 58fab877eec54915a5665f4122b50920f1b9644e.

--- R/data.R
+++ R/data.R
@@ -6,11 +6,3 @@
 #' @format A Biobase::ExpressionSet object
 #' @source \url{http://biorxiv.org/content/biorxiv/early/2016/05/12/052910.full.pdf}
 "GSE14764.eset"
-#' Angiogenic gene signature
-#'
-#' A dataframe consisting of the gene signature
-#' for angiogenic/non-angiogenic ovarian cancer subtyping
-#'
-#' @format A dataframe
-#' @source \url{http://jnci.oxfordjournals.org/cgi/content/full/98/4/262/DC1}
-"sigOvcAngiogenic"
