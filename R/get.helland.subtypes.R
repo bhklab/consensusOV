a39596d0a4ef3e292082a9fb6d284ca90eab3a87
kofiav <victor.kofia@gmail.com>
2017-07-05 09:24:06 -0400

    Format R code to reduce num of lines >80 chars

--- R/get.helland.subtypes.R
+++ R/get.helland.subtypes.R
@@ -1,38 +1,46 @@
 #' Get ovarian cancer subtypes as defined by Helland et al., 2011
-#' 
-#' @param expression.matrix A matrix of gene expression values with rows as genes, columns as samples.
-#' @param entrez.ids A vector of Entrez Gene IDs, corresponding to the rows of \code{expression.matrix}
-#' @return A list with first value \code{Helland.subtypes} containing a factor of subtype names;
-#' and second value \code{subtype.scores} containing a matrix of subtype scores
+#'
+#' @param expression.matrix A matrix of gene expression values with rows as
+#' genes, columns as samples.
+#' @param entrez.ids A vector of Entrez Gene IDs, corresponding to the rows of
+#' \code{expression.matrix}
+#' @return A list with first value \code{Helland.subtypes} containing a factor
+#' of subtype names; and second value \code{subtype.scores} containing a matrix
+#' of subtype scores
 #' @examples
 #' library(Biobase)
 #' data(GSE14764.eset)
 #' expression.matrix <- exprs(GSE14764.eset)
 #' entrez.ids <- as.character(fData(GSE14764.eset)$EntrezGene.ID)
 #' get.helland.subtypes(expression.matrix, entrez.ids)
-#' @references Helland et al. \emph{Deregulation of MYCN, LIN28B and LET7 in a molecular subtype of 
-#' aggressive high-grade serous ovarian cancers.} PloS one (2011).
+#' @references Helland et al. \emph{Deregulation of MYCN, LIN28B and LET7 in a
+#' molecular subtype of aggressive high-grade serous ovarian cancers.}
+#' PloS one (2011).
 #' @export
 get.helland.subtypes <-
 function(expression.matrix, entrez.ids) {
-  
+
   entrez.ids <- as.character(entrez.ids)
-  helland.gene.set <- Reduce(function(x,y) union(x, y), lapply(entrez.id.logFC.list.helland, function (x) x$ENTREZID))
+  helland.gene.set <- Reduce(function(x,y) union(x, y),
+                             lapply(entrez.id.logFC.list.helland,
+                                    function (x) x$ENTREZID))
   intersecting.entrez.ids <- intersect(helland.gene.set, entrez.ids)
-  
+
   # Only keep genes present in both the supplementary and this eset
-  entrez.id.logFC.list <- lapply(entrez.id.logFC.list.helland, function(x) x[x$ENTREZID %in% intersecting.entrez.ids, ])
-  
+  entrez.id.logFC.list <- lapply(
+      entrez.id.logFC.list.helland,
+      function(x) x[x$ENTREZID %in% intersecting.entrez.ids, ])
+
   subtype.scores <- sapply(entrez.id.logFC.list, function(x) {
     ordered.expression.subset <- expression.matrix[match(x$ENTREZID, entrez.ids),]
-    
     return(apply(ordered.expression.subset, 2, function(y) sum((y * x$logFC))))
-    })
+  })
   old.rownames <- rownames(subtype.scores)
   # Scale to mean=0, variance=1
   subtype.scores <- apply(subtype.scores, 2, scale)
   rownames(subtype.scores) <- old.rownames
-  subclasses <- factor(colnames(subtype.scores)[apply(subtype.scores, 1, which.max)], levels=c("C2", "C4", "C5", "C1"))
-  
+  subclasses <- factor(colnames(subtype.scores)[apply(subtype.scores, 1, which.max)],
+                       levels=c("C2", "C4", "C5", "C1"))
+
   return(list(Helland.subtypes=subclasses, subtype.scores=subtype.scores))
 }
--- data-raw/get_sysdata.R
+++ data-raw/get_sysdata.R
@@ -4,29 +4,55 @@ library(hgu133plus2.db)
 
 ## centroids.konecny
 
-konecny.supplementary.data <- gdata::read.xls(system.file("extdata", "jnci_JNCI_14_0249_s05.xls", package="MetaGx"), sheet=4)
+konecny.supplementary.data <- gdata::read.xls(
+    system.file("extdata", "jnci_JNCI_14_0249_s05.xls", package="MetaGx"),
+    sheet=4)
 
 # Subset supplementary.data to consist of centroids with intersecting genes
 
-konecny.supplementary.data <- gdata::read.xls(system.file("extdata", "jnci_JNCI_14_0249_s05.xls", package="MetaGx"), sheet=4)
+konecny.supplementary.data <- gdata::read.xls(
+    system.file("extdata", "jnci_JNCI_14_0249_s05.xls", package="MetaGx"),
+    sheet=4)
 konecny.centroids <- konecny.supplementary.data[,c(2,4:7)]
-konecny.centroids[,2:5] <- sapply(konecny.centroids[,2:5], function(x) ave(x, konecny.centroids$EntrezGeneID, FUN=mean))
+konecny.centroids[,2:5] <- sapply(
+    konecny.centroids[,2:5],
+    function(x) ave(x, konecny.centroids$EntrezGeneID, FUN=mean))
 konecny.centroids <- unique(konecny.centroids)
 rownames(konecny.centroids) <- konecny.centroids$EntrezGeneID
 konecny.centroids <- konecny.centroids[,-1]
 
 ## entrez.id.logFC.list.helland
-helland.supplementary.type.1 <- gdata::read.xls(system.file("extdata", "journal.pone.0018064.s015.XLS", package="MetaGx"), sheet=1)
-helland.supplementary.type.2 <- gdata::read.xls(system.file("extdata", "journal.pone.0018064.s015.XLS", package="MetaGx"), sheet=2)
-helland.supplementary.type.4 <- gdata::read.xls(system.file("extdata", "journal.pone.0018064.s015.XLS", package="MetaGx"), sheet=3)
-helland.supplementary.type.5 <- gdata::read.xls(system.file("extdata", "journal.pone.0018064.s015.XLS", package="MetaGx"), sheet=4)
-helland.supplementary.tables <- list(C1=helland.supplementary.type.1, C2=helland.supplementary.type.2, C4=helland.supplementary.type.4, C5=helland.supplementary.type.5)
+helland.supplementary.type.1 <- gdata::read.xls(
+    system.file("extdata", "journal.pone.0018064.s015.XLS", package="MetaGx"),
+    sheet=1)
+helland.supplementary.type.2 <- gdata::read.xls(
+    system.file("extdata", "journal.pone.0018064.s015.XLS", package="MetaGx"),
+    sheet=2)
+helland.supplementary.type.4 <- gdata::read.xls(
+    system.file("extdata", "journal.pone.0018064.s015.XLS", package="MetaGx"),
+    sheet=3)
+helland.supplementary.type.5 <- gdata::read.xls(
+    system.file("extdata", "journal.pone.0018064.s015.XLS", package="MetaGx"),
+    sheet=4)
+helland.supplementary.tables <- list(
+    C1=helland.supplementary.type.1,
+    C2=helland.supplementary.type.2,
+    C4=helland.supplementary.type.4,
+    C5=helland.supplementary.type.5)
 
-entrez.id.logFC.list.helland <- lapply(helland.supplementary.tables, function(x) {
-  ## Use the supplementary table's listed probe id and gene name to determine the Entrez ID
-  # If there is only one EntrezID that maps to a probe in hgu133plus2.db, use that Entrez ID.
-  # If there are multiple EntrezIDs that map to a probe, then use the EntrezID (if any) that corresponds to the provided gene symbol.
-  current.mapping <- suppressWarnings(AnnotationDbi::select(hgu133plus2.db, as.character(x$ID), c("ENTREZID", "SYMBOL")))
+entrez.id.logFC.list.helland <- lapply(helland.supplementary.tables,
+                                       function(x) {
+  ## Use the supplementary table's listed probe id and gene name to determine
+  # the Entrez ID
+
+  # If there is only one EntrezID that maps to a probe in hgu133plus2.db, use
+  # that Entrez ID.
+
+  # If there are multiple EntrezIDs that map to a probe, then use the EntrezID
+  #(if any) that corresponds to the provided gene symbol.
+  current.mapping <- suppressWarnings(
+      AnnotationDbi::select(hgu133plus2.db, as.character(x$ID),
+                            c("ENTREZID", "SYMBOL")))
   current.mapping <- current.mapping[ !is.na(current.mapping$ENTREZID), ]
   colnames(x)[1:2] <- c("PROBEID", "SYMBOL")
   mappings.with.unique.probeid <- current.mapping[ !(current.mapping$PROBEID %in% current.mapping$PROBEID[duplicated(current.mapping$PROBEID)]),]
@@ -38,16 +64,30 @@ entrez.id.logFC.list.helland <- lapply(helland.supplementary.tables, function(x)
   return(to.return)
 })
 
-helland.gene.set <- Reduce(function(x,y) union(x, y), lapply(entrez.id.logFC.list.helland, function (x) x$ENTREZID))
+helland.gene.set <- Reduce(function(x,y) union(x, y),
+                           lapply(entrez.id.logFC.list.helland,
+                           function (x) x$ENTREZID))
 
 ## verhaak.genesets.entrez.ids
-verhaak.supplementary.data.sheet7 <- read.xls(system.file("extdata", "JCI65833sd1.xls", package="MetaGx"), sheet=7, skip=1)
+verhaak.supplementary.data.sheet7 <- read.xls(
+    system.file("extdata", "JCI65833sd1.xls", package="MetaGx"),
+    sheet=7, skip=1)
 
-verhaak.gene.symbols <- lapply(levels(verhaak.supplementary.data.sheet7$CLASS), function(y) as.character(verhaak.supplementary.data.sheet7[verhaak.supplementary.data.sheet7$CLASS==y,1]))
+verhaak.gene.symbols <- lapply(levels(
+    verhaak.supplementary.data.sheet7$CLASS),
+    function(y) as.character(
+        verhaak.supplementary.data.sheet7[
+            verhaak.supplementary.data.sheet7$CLASS==y,1
+        ]
+    )
+)
+
 
 names(verhaak.gene.symbols) <- levels(verhaak.supplementary.data.sheet7$CLASS)
 
-verhaak.genesets.entrez.ids <- lapply(verhaak.gene.symbols, function(gene.names) do.call(c, as.list(org.Hs.egALIAS2EG)[gene.names]))
+verhaak.genesets.entrez.ids <- lapply(
+    verhaak.gene.symbols,
+    function(gene.names) do.call(c, as.list(org.Hs.egALIAS2EG)[gene.names]))
 
 ## esets.rescaled.classified.filteredgenes
 ## consensus.training.dataset.full
@@ -55,17 +95,28 @@ load("~/Desktop/mar7_ovcsubtypes/OvcSubtypes/reports/esets.not.rescaled.classifi
 
 verhaak.entrez.ids <- unlist(verhaak.genesets.entrez.ids)
 
-esets.rescaled.classified <- lapply(esets.not.rescaled.classified, function(eset) {
-  exprs(eset) <- t(scale(t(exprs(eset))))
-  eset
-})
+esets.rescaled.classified <- lapply(esets.not.rescaled.classified,
+                                    function(eset) {
+                                        exprs(eset) <- t(scale(t(exprs(eset))))
+                                        eset
+                                    }
+                            )
 
-esets.rescaled.classified.filteredgenes <- lapply(esets.not.rescaled.classified, function(eset) {
-  eset <- eset[as.character(fData(eset)$EntrezGene.ID) %in% as.character(verhaak.entrez.ids),]
-  })
+esets.rescaled.classified.filteredgenes <- lapply (
+    esets.not.rescaled.classified,
+    function(eset) {
+        eset <- eset[
+            as.character(fData(eset)$EntrezGene.ID) %in% as.character(verhaak.entrez.ids),
+        ]
+    }
+)
 
 
-consensus.training.dataset.full <- dataset.merging(esets.rescaled.classified.filteredgenes, method = "intersect", standardization = "none")
+consensus.training.dataset.full <- dataset.merging(
+    esets.rescaled.classified.filteredgenes,
+    method = "intersect",
+    standardization = "none"
+)
 
 save(
   "konecny.centroids",
@@ -80,9 +131,13 @@ load("~/Desktop/mar7_ovcsubtypes/OvcSubtypes/reports/esets.not.rescaled.RData")
 GSE14764.eset <- esets.not.rescaled$GSE14764
 
 # Only keep genes relevant for examples
-entrez.id.union <- c(as.character(unlist(lapply(entrez.id.logFC.list.helland, function(x) x$ENTREZID))),
-		     unlist(verhaak.genesets.entrez.ids),
-		     rownames(konecny.centroids))
+entrez.id.union <- c(
+    as.character(
+        unlist(lapply(entrez.id.logFC.list.helland, function(x) x$ENTREZID))
+    ),
+    unlist(verhaak.genesets.entrez.ids),
+    rownames(konecny.centroids)
+)
 
 GSE14764.eset <- GSE14764.eset[fData(GSE14764.eset)$EntrezGene.ID %in% entrez.id.union,]
 
