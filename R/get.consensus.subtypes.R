bhklab/consensusOV

commit 7ac6d253fe7598557d21d38bbf350577a00b5179

Replace 1:length(m) with seq_along(m)

R/get.consensus.subtypes.R
@@ -61,7 +61,7 @@ function(expression.matrix, entrez.ids, .dataset.names.to.keep=names(esets.resca
   train.expression.matrix <- t(exprs(training.dataset)[match(intersecting.entrez.ids, fData(training.dataset)$EntrezGene.ID),])
 
   train.pairwise.matrix <-
-    apply(combn(1:length(intersecting.entrez.ids),2), 2, function(pair) train.expression.matrix[,pair[1]] > train.expression.matrix[,pair[2]])
+    apply(combn(seq_along(length(intersecting.entrez.ids),2)), 2, function(pair) train.expression.matrix[,pair[1]] > train.expression.matrix[,pair[2]])
   train.pairwise.vals <- as.data.frame(train.pairwise.matrix)
 
   rf.model <- randomForest(x=train.pairwise.vals, y=train.labels)
@@ -69,7 +69,7 @@ function(expression.matrix, entrez.ids, .dataset.names.to.keep=names(esets.resca
   test.expression.matrix <- t(expression.matrix[match(intersecting.entrez.ids, entrez.ids),])
 
   test.pairwise.matrix <-
-    apply(combn(1:length(intersecting.entrez.ids),2), 2, function(pair) test.expression.matrix[,pair[1]] > test.expression.matrix[,pair[2]])
+    apply(combn(seq_along(length(intersecting.entrez.ids),2)), 2, function(pair) test.expression.matrix[,pair[1]] > test.expression.matrix[,pair[2]])
   test.pairwise.vals <- as.data.frame(test.pairwise.matrix)
 
   my.predictions <- predict(rf.model, newdata = test.pairwise.vals)

commit 32505e573a2fbb84b71d45ab300cd29eafb718a9

Fix bug introduced in 7ac6d25 causing R CMD Check to fail

R/get.consensus.subtypes.R
@@ -78,7 +78,7 @@ function(expression.matrix,
   ])
 
   train.pairwise.matrix <-
-    apply(combn(seq_along(length(intersecting.entrez.ids),2)), 2,
+    apply(combn(seq_len(length(intersecting.entrez.ids)),2), 2,
           function(pair) {
             train.expression.matrix[,pair[1]] > train.expression.matrix[,pair[2]]
           })
@@ -91,7 +91,7 @@ function(expression.matrix,
   )
 
   test.pairwise.matrix <-
-    apply(combn(seq_along(length(intersecting.entrez.ids),2)), 2,
+    apply(combn(seq_len(length(intersecting.entrez.ids)),2), 2,
           function(pair) {
               test.expression.matrix[,pair[1]] > test.expression.matrix[,pair[2]]
           })